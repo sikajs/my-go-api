875dba6fe5658e334c124228d1e6b3ee19adc707
Jason Shen <sikajs@gmail.com>
2018-08-06 20:54:46 +0800

    Make db connected, be able to insert via go, be able to query single record via go api

--- src/main.go
+++ src/main.go
@@ -12,23 +12,24 @@ import (
 	"github.com/gorilla/mux"
 )
 
+const (
+	host     = "localhost"
+	port     = 5432
+	user     = "pqgotest"
+	password = "password"
+	dbname   = "pqgotest"
+	sslmode  = "disable"
+)
+
+// Post data structure
+type Post struct {
+	id      int
+	title   string
+	content string
+}
+
 func main() {
-	//DB connection
-	connStr := "user=pqgotest dbname=pqgotest"
-	db, err := sql.Open("postgres", connStr)
-	if err != nil {
-		log.Fatal(err)
-	}
-	defer db.Close()
-
-	//DB query
-	var id int
-	err := db.QueryRow("INSERT INTO posts(title,content) VALUES('test', 'desc123') RETURNING id").Scan(&id)
-	fmt.Println(id)
-	if err != nil {
-		log.Fatal(err)
-	}
-
+	// routing
 	var router = mux.NewRouter()
 	router.HandleFunc("/healthcheck", healthCheck).Methods("GET")
 	router.HandleFunc("/message", handleQryMessage).Methods("GET")
@@ -59,19 +60,50 @@ func healthCheck(w http.ResponseWriter, r *http.Request) {
 	json.NewEncoder(w).Encode("Still alive!")
 }
 
-//
+// DB connection
+func dbConnect() {
+	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s", host, port, user, password, dbname, sslmode)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	fmt.Println(`Database connected.`)
+}
 
-//CRUD for post
+// CRUD for post
 func createPost(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	message := vars["msg"]
+
+	//DB connection
+	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s", host, port, user, password, dbname, sslmode)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	fmt.Println(`Database connected.`)
+
+	sqlStatement := `
+	INSERT INTO posts (id, title, content)
+	VALUES ($1, $2, $3)
+	RETURNING id`
+
+	var id int
+
+	dbErr := db.QueryRow(sqlStatement, 5, "insert from go", "nice description").Scan(&id)
+	if dbErr != nil {
+		panic(dbErr)
+	}
+	fmt.Println("New record ID is:", id)
 
 	json.NewEncoder(w).Encode(map[string]string{"create post based on ": message})
 }
 
 func listPosts(w http.ResponseWriter, r *http.Request) {
 	// vars := mux.Vars(r)
-	// message := vars["msg"]
+	// message := vars[""]
 
 	json.NewEncoder(w).Encode(map[string]string{"posts": "list all the posts"})
 }
@@ -80,5 +112,26 @@ func showPost(w http.ResponseWriter, r *http.Request) {
 	vars := mux.Vars(r)
 	key := vars["key"]
 
-	json.NewEncoder(w).Encode(map[string]string{"list the post based on key": key})
+	var p Post
+
+	connStr := fmt.Sprintf("host=%s port=%d user=%s password=%s dbname=%s sslmode=%s", host, port, user, password, dbname, sslmode)
+	db, err := sql.Open("postgres", connStr)
+	if err != nil {
+		log.Fatal(err)
+	}
+	defer db.Close()
+	fmt.Println(`Database connected.`)
+
+	sqlStatement := `SELECT id, title, content FROM posts WHERE id=$1`
+	row := db.QueryRow(sqlStatement, key)
+	switch err := row.Scan(&p.id, &p.title, &p.content); err {
+	case sql.ErrNoRows:
+		fmt.Println("No row were found with key ", key)
+	case nil:
+		fmt.Println(p)
+		json.NewEncoder(w).Encode(map[string]string{"post": p.title})
+	default:
+		panic(err)
+	}
+
 }
